da9ad46d83aaab25f086ede7bc3d813b982368f3
Priya Lama <priyalama709@gmail.com>
2017-12-10 23:47:09 +0530

    first commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,39 +4,39 @@ import { DisplayService } from '../display.service';
 import { Component,OnInit } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import {OnDestroy} from '@angular/core';
-
 @Component({
     selector: 'dashboard',
     templateUrl: './dashboard.component.html',
     // styleUrls: './dashboard.component.css',
     
 })
-
-
-
 // export class DashboardComponent implements OnInit{
-
 //     constructor (private displayService: DisplayService){};
-
 //     ngOnInit():void{
 //         if(this.displayService.query){
 //             console.log(this.displayService.getMovies());
 //         }
 //     }
 // }
-
 export class DashboardComponent implements OnDestroy {
+     
     message: any;
     subscription: Subscription;
-
-    constructor(private messageService: MessageService) {
+    constructor(private messageService: MessageService,private displayService: DisplayService  ) {
         // subscribe to home component messages
         this.subscription = this.messageService.getMessage().subscribe(message => { this.message = message; });
     }
- 
-   
+    
+    
     ngOnDestroy() {
         // unsubscribe to ensure no memory leaks
         this.subscription.unsubscribe();
     }
+    writeToDbms(m:HTMLObjectElement):void{
+        // console.log(m);
+        //  console.log( this.searchService.writeRestaurent(m));
+        //console.log(m);
+        this.displayService.addNewMovie(m);
+        
+        }
 }
--- src/app/display.service.ts
+++ src/app/display.service.ts
@@ -1,30 +1,42 @@
 import { Movie } from './movie';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SearchComponent } from './search/search.component';
-
 import { Injectable } from '@angular/core';
 import { Headers,Http } from '@angular/http';
-
-
 import 'rxjs/add/operator/toPromise';
-
-
 @Injectable()
 export class DisplayService{
     
     query: string = "";
-
     private webUrl = 'https://api.themoviedb.org/3/search/movie?api_key=6f84500dda682130976f8243a46e558a&query=';
-
+    
     constructor (private http: Http){}
-
+    
     setQuery(value){
         this.query=value;
     }
-
     getMovies(): Promise <JSON> {
         return this.http.get(this.webUrl+this.query).toPromise()
                 .then(response => response.json()['results'] );                
     }
-
+    addNewMovie(m): Promise<void> {
+        let headers = new Headers({'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'});
+        console.log("from display service");
+        console.log(m);
+        //console.log(m.restaurant.id);
+        return this.http.post('http://localhost:8080/v1.0/movieservice/Movies',JSON.stringify({
+                        id: m.id,
+                      movieName: m.original_title
+                        //featured_image:res.restaurant.featured_image,
+                       // cuisines:res.restaurant.cuisines,
+                       // average_cost_for_two:res.restaurant.average_cost_for_two,
+                        //rating:res.restaurant.user_rating.aggregate_rating,
+                       // comments:null
+                      }),{headers:headers})
+                 .toPromise()
+                 .then(
+                       (err)=>console.log(err))
+                 .catch((ress) =>console.log("error caught",ress))
+        }
 }
